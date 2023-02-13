87cf19ccb549edea6ee32c921fb08575c9fc5552
Eren <erencan_inanci@hotmail.com>
2023-02-14 02:18:31 +0300

    dynamic color and emoji change with voting

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "../styles/Joke.css";
+import { getColor, getEmoji } from "../helpers/Helper";
 
 export default function Joke(props) {
+  let emoji = getEmoji(props.votes);
   return (
     <div className="Joke">
       <div className="Joke-buttons">
         <i className="fas fa-arrow-up" onClick={props.upvote}></i>
-        <span className="Joke-votes">{props.votes}</span>
+        <span
+          className="Joke-votes"
+          style={{ borderColor: `${getColor(props.votes)}` }}
+        >
+          {props.votes}
+        </span>
         <i className="fas fa-arrow-down" onClick={props.downvote}></i>
       </div>
       <div className="Joke-text">{props.joke}</div>
 
       <div className="Joke-smiley">
         <i
-          class="em em-rolling_on_the_floor_laughing"
+          class={emoji}
           aria-role="presentation"
           aria-label="ROLLING ON THE FLOOR LAUGHING"
         ></i>
